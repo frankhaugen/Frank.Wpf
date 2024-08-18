773d88efdcbd0f84c2efb1f1b98a4fcced9dd267
Frank R. Haugen <frank.haugen@gmail.com>
2024-08-18 21:00:11 +0200

    Really good XML Rendering acheinved

--- Frank.Wpf.Controls.XmlRenderer/Internals/XmlTreeViewFactory.cs
+++ Frank.Wpf.Controls.XmlRenderer/Internals/XmlTreeViewFactory.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Linq;
+using System.Windows.Controls;
 using System.Xml.Linq;
 
 namespace Frank.Wpf.Controls.XmlRenderer.Internals;
@@ -12,47 +13,63 @@ public class XmlTreeViewFactory
 
         if (rootElement != null)
         {
-            var rootItem = CreateTreeViewItemFromElement(rootElement);
+            var rootItem = CreateTreeViewItem(rootElement);
             treeView.Items.Add(rootItem);
         }
 
         return treeView;
     }
 
-    private static TreeViewItem CreateTreeViewItemFromElement(XElement element)
+    private static TreeViewItem CreateTreeViewItem(XElement element)
     {
-        var treeViewItem = new TreeViewItem
-        {
-            Header = element.Name.LocalName,
-            IsExpanded = true
-        };
+        var treeViewItem = CreateBasicTreeViewItem(element);
 
         // Add attributes
+        AddAttributesToTreeViewItem(element, treeViewItem);
+
+        // Add child elements or text content
+        if (element.HasElements)
+        {
+            AddChildElementsToTreeViewItem(element, treeViewItem);
+        }
+        else if (!string.IsNullOrWhiteSpace(element.Value))
+        {
+            AddTextToTreeViewItem(element, treeViewItem);
+        }
+
+        return treeViewItem;
+    }
+
+    private static void AddAttributesToTreeViewItem(XElement element, TreeViewItem treeViewItem)
+    {
         foreach (var attribute in element.Attributes())
         {
             var attributeItem = CreateTreeViewItemFromAttribute(attribute);
             treeViewItem.Items.Add(attributeItem);
         }
+    }
 
-        // Add child elements or text content
-        if (element.HasElements)
+    private static void AddChildElementsToTreeViewItem(XElement element, TreeViewItem treeViewItem)
+    {
+        foreach (var childElement in element.Elements())
         {
-            foreach (var childElement in element.Elements())
-            {
-                var childItem = CreateTreeViewItemFromElement(childElement);
-                treeViewItem.Items.Add(childItem);
-            }
+            var childItem = CreateTreeViewItem(childElement);
+            treeViewItem.Items.Add(childItem);
         }
-        else if (!string.IsNullOrWhiteSpace(element.Value))
-        {
-            var textItem = new TreeViewItem
-            {
-                Header = element.Value.Trim()
-            };
-            treeViewItem.Items.Add(textItem);
-        }
+    }
 
-        return treeViewItem;
+    private static void AddTextToTreeViewItem(XElement element, TreeViewItem treeViewItem)
+    {
+        // Add the text content directly to the TreeViewItem header if no child elements exist
+        treeViewItem.Header = $"{treeViewItem.Header}: {element.Value.Trim()}";
+    }
+
+    private static TreeViewItem CreateBasicTreeViewItem(XElement element)
+    {
+        return new TreeViewItem
+        {
+            Header = element.Name.LocalName
+        };
     }
 
     private static TreeViewItem CreateTreeViewItemFromAttribute(XAttribute attribute)
